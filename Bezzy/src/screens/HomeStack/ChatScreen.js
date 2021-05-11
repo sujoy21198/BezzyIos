c65e7b86a2c111f1c3f7f62346e633543f26aee1
Sourendro Banerjee <sourendro@webstep.in>
2021-05-11 23:37:10 +0530

    ipa of 11th

--- Bezzy/src/screens/HomeStack/ChatScreen.js
+++ Bezzy/src/screens/HomeStack/ChatScreen.js
@@ -16,41 +16,7 @@ import {
 } from '../../../styles/MessageStyles';
 
 const Messages = [
-    {
-        id: '1',
-        userName: 'Jenny Doe',
-        messageTime: '4 mins ago',
-        messageText:
-            'Hey there, this is my test for a post of my social app in React Native.',
-    },
-    {
-        id: '2',
-        userName: 'John Doe',
-        messageTime: '2 hours ago',
-        messageText:
-            'Hey there, this is my test for a post of my social app in React Native.',
-    },
-    {
-        id: '3',
-        userName: 'Ken William',
-        messageTime: '1 hours ago',
-        messageText:
-            'Hey there, this is my test for a post of my social app in React Native.',
-    },
-    {
-        id: '4',
-        userName: 'Selina Paul',
-        messageTime: '1 day ago',
-        messageText:
-            'Hey there, this is my test for a post of my social app in React Native.',
-    },
-    {
-        id: '5',
-        userName: 'Christy Alex',
-        messageTime: '2 days ago',
-        messageText:
-            'Hey there, this is my test for a post of my social app in React Native.',
-    },
+    
 ];
 
 export default class ChatScreen extends React.Component {
