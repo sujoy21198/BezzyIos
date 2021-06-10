0b94a42f3cb951b30aff35dd606e8e7827e57fcd
Anirban Hazra <anirban.hazra@webstep.in>
2021-06-10 10:31:16 +0530

    Keyboard-textfield issue fixing in comment & thread comments

--- Bezzy/src/screens/HomeStack/CommentScreen.js
+++ Bezzy/src/screens/HomeStack/CommentScreen.js
@@ -105,171 +105,177 @@ export default class CommentScreen extends React.Component {
             <Header isBackButton isHomeStackInnerPage headerText={"Comments"} navigation={this.props.navigation} commentCount={(this.props.route.params && this.props.route.params.type !== "otherUserPost") ? this.state.comments.length : undefined} />
             <KeyboardAwareScrollView
                 keyboardShouldPersistTaps="handled"
-                style={{flex: 1}}
             >
-                {
-                    this.state.comments.length > 0 &&
-                    <FlatList
-                    data={this.state.comments}
-                    keyExtractor={item => item.comment_id}
-                    ItemSeparatorComponent={() => <View style={{height: heightToDp("3%")}}/>}
-                    ListFooterComponent={<View style={{height: heightToDp("2%")}}/>}
-                    ListHeaderComponent={<View style={{height: heightToDp("3%")}}/>}
-                    renderItem={({item, index}) => (
-                        <>
-                            <View
-                                style={{
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                    paddingHorizontal: widthToDp("2%")
-                                }}
-                            >
-                                <Image
-                                    source={{uri: item.userimage}}
-                                    style={{height: heightToDp("5%"), width: widthToDp("10%"), borderRadius: 25}}
-                                />
+                <View
+                    style={{height: heightToDp("91.5%")}}
+                >
+                    {
+                        this.state.comments.length > 0 &&
+                        <FlatList
+                        data={this.state.comments}
+                        keyExtractor={item => item.comment_id}
+                        ref={ref => this.refList = ref}
+                        ItemSeparatorComponent={() => <View style={{height: heightToDp("3%")}}/>}
+                        ListFooterComponent={<View style={{height: heightToDp("2%")}}/>}
+                        ListHeaderComponent={<View style={{height: heightToDp("3%")}}/>}
+                        renderItem={({item, index}) => (
+                            <>
                                 <View
                                     style={{
-                                        marginLeft: widthToDp("3%"),
-                                        padding: widthToDp("2%"),
-                                        backgroundColor: 'rgba(0, 255, 255, 0.1)',
-                                        borderRadius: 10,
-                                        width: widthToDp("82.5%")
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        paddingHorizontal: widthToDp("2%")
                                     }}
                                 >
-                                    <Text
+                                    <Image
+                                        source={{uri: item.userimage}}
+                                        style={{height: heightToDp("5%"), width: widthToDp("10%"), borderRadius: 25}}
+                                    />
+                                    <View
                                         style={{
-                                            width: widthToDp("100%"),
-                                            color: '#1b1b1b',
-                                            fontWeight: 'bold',
-                                            fontSize: widthToDp("3.5%")
+                                            marginLeft: widthToDp("3%"),
+                                            padding: widthToDp("2%"),
+                                            backgroundColor: 'rgba(0, 255, 255, 0.1)',
+                                            borderRadius: 10,
+                                            width: widthToDp("82.5%")
                                         }}
-                                    >{item.username}</Text>
-                                    <Text
-                                        style={{
-                                            color: '#1b1b1b',
-                                            fontSize: widthToDp("3%"),
-                                            marginTop: heightToDp("1%")
-                                        }}
-                                    >{item.commentText}</Text>
-                                </View>                            
-                            </View>
-                            <View
-                                style={{
-                                    marginLeft: widthToDp("15%"),
-                                    marginTop: heightToDp("0.8%"),
-                                    width: widthToDp("82%"),
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between', 
-                                    alignItems: 'center'
-                                }}
-                            >
-                                <Text
-                                    style={{
-                                        color: '#808080',
-                                        fontSize: widthToDp("3%")
-                                    }}
-                                >{item.postcomment_time}</Text>
+                                    >
+                                        <Text
+                                            style={{
+                                                width: widthToDp("100%"),
+                                                color: '#1b1b1b',
+                                                fontWeight: 'bold',
+                                                fontSize: widthToDp("3.5%")
+                                            }}
+                                        >{item.username}</Text>
+                                        <Text
+                                            style={{
+                                                color: '#1b1b1b',
+                                                fontSize: widthToDp("3%"),
+                                                marginTop: heightToDp("1%")
+                                            }}
+                                        >{item.commentText}</Text>
+                                    </View>                            
+                                </View>
                                 <View
                                     style={{
-                                        position: 'absolute',
-                                        top: heightToDp("0%"),
-                                        right: widthToDp("0%"),
+                                        marginLeft: widthToDp("15%"),
+                                        marginTop: heightToDp("0.8%"),
+                                        width: widthToDp("82%"),
                                         flexDirection: 'row',
+                                        justifyContent: 'space-between', 
                                         alignItems: 'center'
                                     }}
                                 >
-                                    <TouchableOpacity
-                                        activeOpacity={0.7}
-                                        onPress={() => this.likeDislikeComment(item)}
+                                    <Text
+                                        style={{
+                                            color: '#808080',
+                                            fontSize: widthToDp("3%")
+                                        }}
+                                    >{item.postcomment_time}</Text>
+                                    <View
+                                        style={{
+                                            position: 'absolute',
+                                            top: heightToDp("0%"),
+                                            right: widthToDp("0%"),
+                                            flexDirection: 'row',
+                                            alignItems: 'center'
+                                        }}
                                     >
-                                        {
-                                            item.login_user_like_comment === "Yes" ?
-                                            <Icon1
-                                                name="heart"
-                                                color={"#ff0000"}
-                                                size={15}
-                                            /> :
-                                            <Icon
-                                                name="heart"
-                                                color={"#69abff"}
-                                                size={15}
-                                            />
-                                        }
-                                    </TouchableOpacity>
-                                    <Text
-                                        style={{
-                                            color: "#777",
-                                            fontSize: widthToDp("3.5%"),
-                                            paddingLeft: widthToDp("2%")
-                                        }}
-                                    >{item.total_like_on_comment}</Text>
-                                    <Icon
-                                        name="comment"
-                                        color="#69abff"
-                                        size={15}
-                                        style={{paddingLeft: widthToDp("4%")}}
-                                        onPress={() => this.threadComment(item)}
-                                    />
-                                    <Text
-                                        style={{
-                                            color: "#777",
-                                            fontSize: widthToDp("3.5%"),
-                                            paddingLeft: widthToDp("2%")
-                                        }}
-                                    >{item.total_comment_on_comment}</Text>
-                                </View> 
-                            </View>
-                        </>
-                    )}
-                    />
-                }     
-            </KeyboardAwareScrollView> 
-            <View
-                style={{
-                    backgroundColor: '#fff',
-                    padding: widthToDp("1%"),
-                }}
-            >
+                                        <TouchableOpacity
+                                            activeOpacity={0.7}
+                                            onPress={() => this.likeDislikeComment(item)}
+                                        >
+                                            {
+                                                item.login_user_like_comment === "Yes" ?
+                                                <Icon1
+                                                    name="heart"
+                                                    color={"#ff0000"}
+                                                    size={15}
+                                                /> :
+                                                <Icon
+                                                    name="heart"
+                                                    color={"#69abff"}
+                                                    size={15}
+                                                />
+                                            }
+                                        </TouchableOpacity>
+                                        <Text
+                                            style={{
+                                                color: "#777",
+                                                fontSize: widthToDp("3.5%"),
+                                                paddingLeft: widthToDp("2%")
+                                            }}
+                                        >{item.total_like_on_comment}</Text>
+                                        <Icon
+                                            name="comment"
+                                            color="#69abff"
+                                            size={15}
+                                            style={{paddingLeft: widthToDp("4%")}}
+                                            onPress={() => this.threadComment(item)}
+                                        />
+                                        <Text
+                                            style={{
+                                                color: "#777",
+                                                fontSize: widthToDp("3.5%"),
+                                                paddingLeft: widthToDp("2%")
+                                            }}
+                                        >{item.total_comment_on_comment}</Text>
+                                    </View> 
+                                </View>
+                            </>
+                        )}
+                        />
+                    }  
+                </View>   
                 <View
                     style={{
-                        paddingHorizontal: widthToDp("2%"),
-                        width: widthToDp("98%"),
-                        borderWidth: 1,
-                        borderRadius: 10,
-                        borderColor: "#69abff",
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center'
+                        backgroundColor: '#fff',
+                        padding: widthToDp("1%"),
+                        height: heightToDp("5%")
                     }}
                 >
-                    <TextInput
-                        placeholder="Enter message"
-                        placeholderTextColor="#808080"
+                    <View
                         style={{
-                            width: widthToDp("88%"),
-                            paddingHorizontal: widthToDp("1%"),
-                            paddingVertical: heightToDp("0%"),
-                            fontSize: widthToDp("4.3%"),
-                            color: '#777'
+                            paddingHorizontal: widthToDp("2%"),
+                            width: widthToDp("98%"),
+                            borderWidth: 1,
+                            borderRadius: 10,
+                            borderColor: "#69abff",
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center'
                         }}
-                        multiline
-                        ref={ref => this.refChatField = ref}
-                        onChangeText={text => this.setState({commentText: text})}
-                    />
-                    <TouchableOpacity
-                        onPress={this.sendComment}
-                        style={{width: widthToDp("12%")}}
-                        disabled={this.state.commentText.trim() === "" || this.state.isSendingComment}
                     >
-                        <Ionicons
-                        name={Platform.OS==='android' ? 'md-send' : 'ios-send'}
-                        size={20}
-                        color="#69abff"
+                        <TextInput
+                            placeholder="Enter message"
+                            placeholderTextColor="#808080"
+                            style={{
+                                width: widthToDp("88%"),
+                                paddingHorizontal: widthToDp("1%"),
+                                paddingVertical: heightToDp("0%"),
+                                fontSize: widthToDp("4.3%"),
+                                color: '#777',
+                            }}
+                            multiline
+                            ref={ref => this.refChatField = ref}
+                            onChangeText={text => this.setState({commentText: text})}
                         />
-                    </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={this.sendComment}
+                            style={{width: widthToDp("12%")}}
+                            disabled={this.state.commentText.trim() === "" || this.state.isSendingComment}
+                        >
+                            <Ionicons
+                            name={Platform.OS==='android' ? 'md-send' : 'ios-send'}
+                            size={20}
+                            color="#69abff"
+                            />
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
+            </KeyboardAwareScrollView> 
+            
             <RBSheet
                 ref={ref => {
                     this.RBSheet = ref;
