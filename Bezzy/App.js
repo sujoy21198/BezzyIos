99bc06cf1406b9bbafefcf8d5da8e52148ee2bc1
Sourendro Banerjee <sourendro@webstep.in>
2021-06-15 14:05:41 +0530

    changes done

--- Bezzy/App.js
+++ Bezzy/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Root } from 'native-base'
 import Navigation from './src/Navigation';
+import RNFirebase from '@react-native-firebase/app';
 
+const configOptions = {
+  debug: true,
+  promptOnMissingPlayServices: true
+}
+
+export const firebase = RNFirebase.initializeApp(configOptions);
 
 export default class App extends Component {
   render() {
--- Bezzy/src/components/PushNotificationController.js
+++ Bezzy/src/components/PushNotificationController.js
@@ -7,53 +7,39 @@ const PushNotificationController = (props) => {
     useEffect(() => {
         PushNotification.configure({
             // (optional) Called when Token is generated (iOS and Android)
-            onRegister: function(token) {
+            onRegister: function (token) {
                 console.log('TOKEN:', token)
-            },
 
-            // (required) Called when a remote or local notification is opened or received
-            onNotification: async function(notification) {
-                console.log('REMOTE NOTIFICATION ==>', notification)   
 
                 PushNotification.createChannel(
                     {
                         id: "123",
-                        channelId: notification.channelId, 
-                        channelName: "My channel", 
-                        channelDescription: "A channel to categorise your notifications", 
-                        playSound: false, 
-                        soundName: "default", 
-                        importance: Importance.HIGH, 
-                        vibrate: true, 
+                        channelId: "1",
+                        channelName: "My channel",
+                        channelDescription: "A channel to categorise your notifications",
+                        playSound: false,
+                        soundName: "default",
+                        importance: Importance.HIGH,
+                        vibrate: true,
                     },
-                    (created) => console.log(`createChannel returned '${created}'`) 
+                    (created) => console.log(`createChannel returned '${created}'`)
                 );
+            },
+
+            // (required) Called when a remote or local notification is opened or received
+            onNotification: async function (notification) {
+                console.log('REMOTE NOTIFICATION ==>', notification)
+
                 PushNotification.localNotification({
                     channelId: notification.channelId,
                     title: notification.title,
                     message: notification.message,
                 })
 
-                if(notification.userInteraction) {            
-                    let notificationData = JSON.parse(await AsyncStorage.getItem("notificationData"));
-                    console.warn(notificationData);
-                    if(notificationData.type === "post") {
-                        props.navigation.navigate("ImagePreviewScreen", {
-                            image: {post_id: notificationData.respostID}
-                        });
-                    } else if(notificationData.type === "chat_box_msg") {
-                        props.navigation.navigate("InboxScreen", {
-                            friendId: notificationData.from_userid,
-                            friendName: notificationData.from_usernam,
-                            friendImage: notificationData.from_userimage
-                        });
-                    } else {
-                        props.navigation.navigate("HomeScreen")
-                    }
-                    PushNotification.deleteChannel(notification.channelId)
-                    AsyncStorage.removeItem("notificationData")
+                if(notification.userInteraction) {  
+                    props.navigation.navigate("HomeScreen")
                 } else {
-                    AsyncStorage.setItem("notificationData", JSON.stringify(notification.data));
+                    //AsyncStorage.setItem("notificationData", JSON.stringify(notification.data));
                 }
             },
             // Android only: GCM or FCM Sender ID
